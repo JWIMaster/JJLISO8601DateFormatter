f94853d7b7eccb0425a669adafff969f57597855
J_W_I_ <64947174+JWIMaster@users.noreply.github.com>
2025-11-09 21:49:22 +1100

    Specify platform support in Package.swift
    Added platform support for iOS, tvOS, and macOS.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "JJLISO8601DateFormatter",
+    platforms: [
+        .iOS("7.0"),
+        .tvOS(.v11),
+        .macOS(.v10_13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
